cc8bfcdaa3c5dd6d53dfcf1a13a33c9f5ec298b5
Audrey <43081195+Audi-Un-Autre@users.noreply.github.com>
2018-10-01 14:32:16 -0400

    Environment, Navmesh, & Script Updates

---
+++ Labrynth/Assets/SCRIPTS/PLAYER_MOV.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLAYER_MOV : MonoBehaviour {
+
+    Vector3 targetDestination;
+    Vector3 playerPosition;
+    Vector3 destination;
+    [SerializeField] Rigidbody rb;
+
+    [SerializeField] bool clicked;
+    float maxSpeed;
+    float stopRadius;
+
+    void Start()
+    {
+
+        stopRadius = .5f;
+        maxSpeed = 20f;
+    }
+
+    void Update()
+    {
+
+        MouseClicked();
+
+        if (clicked)
+        {
+            SetDestination();
+            GoToDestination();
+        }
+
+    }
+
+    // CHECK IF PLAYER IS CLICKING
+    private void MouseClicked()
+    {
+
+        // GET MOUSE CLICK POSITION WHEN CLICKED, IF CLICKED ON THE FLOOR
+        RaycastHit hit;
+        if (Input.GetMouseButton(0) && Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+        {
+            clicked = true;
+            if (hit.collider.tag == "Floor")
+            {
+                Debug.Log(hit.point);
+                targetDestination = hit.point;
+            }
+        }
+    }
+
+    // MOVE PLAYER TO CLICK LOCATION
+    private void SetDestination()
+    {
+
+        // CREATE PATH TO DESTINATION
+        destination = targetDestination - transform.position;
+        transform.LookAt(targetDestination);
+    }
+
+    // CHECK IF ARRIVED TO DESTINATION
+    private void GoToDestination()
+    {
+
+        // MOVE
+        Debug.Log(destination.magnitude);
+        if (destination.magnitude > stopRadius)
+        {
+            destination.Normalize();
+            destination *= maxSpeed;
+            rb.velocity = destination;
+        }
+
+        // STOP
+        else
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            clicked = false;
+        }
+    }
+}
