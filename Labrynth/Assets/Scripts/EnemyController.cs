f701fffabf8b76f71cb5756278119a38202319ce
Audrey <43081195+Audi-Un-Autre@users.noreply.github.com>
2018-10-17 10:55:18 -0400

    Upload Models

---
+++ Labrynth/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Transform enemy;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float detectionDistance;
+    [SerializeField] private float attackDistance;
+    [SerializeField] Rigidbody rb;
+    static Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Vector3.Distance(target.position, this.transform.position) < detectionDistance) // MINIMUM DISTANCE TO START FOLLOW
+        {
+            Vector3 direction = target.position - enemy.position;
+            if (direction.magnitude < attackDistance) // RADIUS OF SATISFACTION
+            {
+                enemy.Translate(0.0f, 0.0f, 0.0f);
+                anim.SetBool("isAttacking", true);
+                anim.SetBool("isMoving", false);
+                // STOP MOVEMENT
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+
+            }
+            else if (direction.magnitude > attackDistance && !anim.GetCurrentAnimatorStateInfo(0).IsName("attack"))
+            {
+                anim.SetBool("isMoving", true);
+                // START MOVEMENT
+                direction.Normalize();
+                direction *= moveSpeed;
+                rb.velocity = direction;
+                this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+                anim.SetBool("isMoving", true);
+                anim.SetBool("isAttacking", false);
+            }
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+            anim.SetBool("isAttacking", false);
+            // STOP MOVEMENT
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+}
---
+++ Labrynth/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour {
+
+    [SerializeField] private float maxHealth;
+    [SerializeField] private float currHealth;
+
+	// Use this for initialization
+	void Start () {
+        currHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void HurtPlayer(int damage)
+    {
+        currHealth -= damage;
+    }
+
+    public void HealPlayer(int healAmount)
+    {
+        currHealth += healAmount;
+        if (currHealth > maxHealth)
+        {
+            currHealth = maxHealth;
+        }
+
+    }
+}
