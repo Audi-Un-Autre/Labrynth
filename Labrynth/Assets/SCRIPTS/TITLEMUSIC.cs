33b6ea6f72e88d5d1863ed171ae0541fdfa46421
Audrey <43081195+Audi-Un-Autre@users.noreply.github.com>
2018-12-03 19:16:44 -0500

    Music Persistency

---
+++ Labrynth/Assets/SCRIPTS/TITLEMUSIC.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TITLEMUSIC : MonoBehaviour {
+
+    void Awake()
+    {
+        GameObject[] musics = GameObject.FindGameObjectsWithTag("TitleMusic");
+        if (musics.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "GAME TEST")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
